5f6af42b0b6395905f3679308b254d449012ce08
Ludovic Mermod <ludovic.mermod@epfl.ch>
2024-10-28 14:14:42 +0100

    Some try

---
+++ askAi.md
@@ -0,0 +1,14 @@
+You are an epxert in video game and website design
+You need to build a website for an EPFL association that's called game star
+The goal of the website is to present the association, it's activities and give a way to student to join us
+We are a comitee of 6 persons, the website should include image and small description of us
+The student in EPFL will be technical engeneers and are between 17 and 25 years old
+We do multiple events such as : 
+ICeLAN
+Problem solving in video game
+Couch project
+and more 
+It's important that the site is in 2 languages, has a dark and light mode and is responsive
+Use only html css and maybe js but no extra plugins, the site must be fully responsive
+This is the graphical chart that you should use for the website 
+#133610 #212717 #826343 #cc9a48 #dfdfdf #161616
---
+++ my-react-app/src/App.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from 'react';
+import { createDirectus, rest, readItems } from '@directus/sdk';
+
+const client = createDirectus('http://localhost:8000/directus').with(rest());
+
+const App = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await client.request(readItems('articles'));
+        setArticles(response.data);
+      } catch (error) {
+        console.error('Error fetching data from Directus:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Articles</h1>
+      <ul>
+        {articles.map(article => (
+          <li key={article.id}>{article.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
+/*
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+*/
